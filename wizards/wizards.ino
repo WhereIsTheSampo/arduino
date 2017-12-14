83002d0e4682bcc5f0cbd2e53e8c85ec190de4b4
Bob Reid <rjreid3000@gmail.com>
2017-12-13 21:41:24 -0700

    major refactor to simplify

--- wizards/wizards.ino
+++ wizards/wizards.ino
@@ -3,7 +3,7 @@
 // Inspired by and adapted from SparkFun Addressable RGB LED Sound
 // and Music Visualizer Tutorial Arduino Code by Michael Bartlett
 
-// ##### Libraries #####
+// ##### Libraries ###############
 
 #include <Adafruit_NeoPixel.h>  //Library to simplify interacting with the LED strip
 #ifdef __AVR__
@@ -11,20 +11,22 @@
 #endif                   //More info: http://www.nongnu.org/avr-libc/user-manual/group__avr__power.htlm
 
 
-// ##### Constants #####
+// ##### Constants ###############
 
 const uint16_t LED_PIN        = A5;  // Signal pin for LED strip
+const uint16_t ENVELOPE_PIN   = A0;  // Pin receiving output of sound detector
+const uint16_t KNOB_PIN       = A1;  // Pin receiving output of sensitivity pot
+const uint16_t PALETTE_PIN    = 6;   //Button 1 cycles color palettes
+const uint16_t VISUAL_PIN     = 5;   //Button 2 cycles visualization modes
+
 const uint16_t LED_LENGTH     = 60;  // Total # of LEDs in strip
 const uint16_t LED_HALF       = LED_LENGTH / 2;  // Half of LED strip
 const uint16_t LED_OFFSET     = 7;   // Offset center point of strip to fit installation
-const uint16_t ENVELOPE_PIN   = A0;  // Pin receiving output of sound detector
-const uint16_t KNOB_PIN       = A1;  // Pin receiving output of sensitivity pot
+
 const uint16_t MAX_BRIGHTNESS = 25;  // Max LED brightness (0-255)
 const uint16_t NOISE_FLOOR    = 15;  // Minimum volume threshold
-const uint16_t VISUALS        = 3;
-const uint16_t BUTTON_1       = 6;   //Button 1 cycles color palettes
-const uint16_t BUTTON_2       = 5;   //Button 2 cycles visualization modes
-
+const uint16_t VISUALS        = 3;   // Total number of visualizations     
+const uint16_t PALETTE_THRESHOLD = 1536; 
 
 // not a constant, but used to create constants
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_LENGTH, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -39,22 +41,14 @@ const uint32_t WHITE   = strip.Color(MAX_BRIGHTNESS, MAX_BRIGHTNESS, MAX_BRIGHTN
 const uint32_t OFF     = strip.Color(0, 0, 0);
 
 
-// ##### Globals #####
+// ##### Globals ###############
 
 float volume = 0.0;
-float avg_volume = 0.0; 
-float max_volume = 0.0;
+float avg_vol = 0.0; 
+float max_vol = 0.0;
 float knob = 0.0;
 bool bump = false;     //Used to pass if there was a "bump" in volume
 uint16_t gradient = 0; //Used to iterate and loop through each color palette gradually
-//  This array holds the "threshold" of each color function (i.e. the largest number they take before repeating).
-//  The values are in the same order as in ColorPalette()'s switch case (Rainbow() is first, etc). This is simply to
-//  keep "gradient" from overflowing, the color functions themselves can take any positive value. For example, the
-//  largest value Rainbow() takes before looping is 1529, so "gradient" should reset after 1529, as listed.
-//     Make sure you add/remove values accordingly if you add/remove a color function in the switch-case in ColorPalette().
-uint16_t thresholds[] = {1529, 512};
-uint8_t palette = 0;  //Holds the current color palette.
-uint8_t visual = 0;   //Holds the current visual being displayed.
 
 
 //For Traffic() visual
@@ -64,10 +58,11 @@ uint8_t rgb[LED_LENGTH][3] = {0};  //Stores each dot's specific RGB values.
 float time_bump = 0; //Holds the time (in runtime seconds) the last "bump" occurred.
 float avg_time = 0;  //Holds the "average" amount of time between each "bump" (used for pacing the dot's movement).
 
-uint8_t last = 0;     //Holds the value of volume from the previous loop() pass.
+uint8_t last_vol = 0;     //Holds the value of volume from the previous loop() pass.
 
+uint32_t debug_bump_count = 0;
 
-// ##### Standard Functions #####
+// ##### Standard Functions ###############
 
 void setup()
 {
@@ -75,12 +70,13 @@ void setup()
 
     strip.begin();
 
-  //Defines the buttons pins to be input.
-  pinMode(BUTTON_1, INPUT); pinMode(BUTTON_2, INPUT); 
+    //Defines the buttons pins to be input.
+    pinMode(PALETTE_PIN, INPUT); 
+    pinMode(VISUAL_PIN, INPUT); 
 
-  //Write a "HIGH" value to the button pins.
-  digitalWrite(BUTTON_1, HIGH); digitalWrite(BUTTON_2, HIGH);
-
+    //Write a "HIGH" value to the button pins.
+    digitalWrite(PALETTE_PIN, HIGH); 
+    digitalWrite(VISUAL_PIN, HIGH);
 
     // quick test to ensure strip is working
     ColorWipe(WHITE, 10);
@@ -98,143 +94,76 @@ void loop()
     volume = analogRead(ENVELOPE_PIN);    //Record the volume level from the sound detector
     knob = analogRead(KNOB_PIN) / 1023.0; //Record how far the trimpot is twisted
 
-    if (volume < avg_volume / 2.0 || volume < NOISE_FLOOR)
+    if (volume < avg_vol / 2.0 || volume < NOISE_FLOOR)
     {
         volume = 0.0;
     }
     else
     {
-        avg_volume = (avg_volume + volume ) / 2.0;
+        avg_vol = (avg_vol + volume ) / 2.0;
     }
     
-    max_volume = max(max_volume, volume);
+    max_vol = max(max_vol, volume);
 
+    //This is where "gradient" is modulated to prevent overflow.
+    if (gradient > PALETTE_THRESHOLD) 
+    {
+        gradient %= PALETTE_THRESHOLD;
+        
+        //Everytime a palette gets completed is a good time to readjust "maxVol," just in case
+        //  the song gets quieter; we also don't want to lose brightness intensity permanently
+        //  because of one stray loud sound.
+        max_vol = (max_vol + volume) / 2.0;
+        Serial.println("Max Vol Reset");
+    }
 
-  CyclePalette();  //Changes palette for shuffle mode or button press.
+    //If there is a decent change in volume since the last pass, average it into "avgBump"
 
-  CycleVisual();   //Changes visualization for shuffle mode or button press.
+    float delta_vol = volume - last_vol;
+    
+    if (delta_vol > 10)
+    {
+        avg_time = (avg_time + delta_vol) / 2.0;
+    }
+    
+    //If there is a notable change in volume, trigger a "bump"
+    //  avgbump is lowered just a little for comparing to make the visual slightly more sensitive to a beat.
+    bump = (delta_vol > avg_time * .9);  
+    
+    //If a "bump" is triggered, average the time between bumps
+    if (bump)
+    {
+        debug_bump_count++;
+        Serial.println("BUMP # " + String(debug_bump_count));
+        avg_time = (((millis() / 1000.0) - time_bump) + avg_time) / 2.0;
+        time_bump = millis() / 1000.0;
+    }
 
+//    Serial.println(
+//        "Vol: " + String(volume) + 
+//        ", Avg: " + String(avg_vol) + 
+//        ", Max: " + String(max_vol) + 
+//        ", Knob: " + String(knob));
+
+    VisualizeTraffic();
   
+    gradient++;    //Increments gradient
 
-  //This is where "gradient" is modulated to prevent overflow.
-  if (gradient > thresholds[palette]) {
-    gradient %= thresholds[palette] + 1;
+    last_vol = volume; //Records current volume for next pass
 
-    //Everytime a palette gets completed is a good time to readjust "maxVol," just in case
-    //  the song gets quieter; we also don't want to lose brightness intensity permanently
-    //  because of one stray loud sound.
-    max_volume = (max_volume + volume) / 2.0;
-  }
-
-  //If there is a decent change in volume since the last pass, average it into "avgBump"
-  if (volume - last > 10) avg_time = (avg_time + (volume - last)) / 2.0;
-
-  //If there is a notable change in volume, trigger a "bump"
-  //  avgbump is lowered just a little for comparing to make the visual slightly more sensitive to a beat.
-  bump = (volume - last > avg_time * .9);  
-
-  //If a "bump" is triggered, average the time between bumps
-  if (bump) {
-    avg_time = (((millis() / 1000.0) - time_bump) + avg_time) / 2.0;
-    time_bump = millis() / 1000.0;
-  }
-
-
-  Visualize();
-  
-
-      gradient++;    //Increments gradient
-
-  last = volume; //Records current volume for next pass
-
-  delay(30);     //Paces visuals so they aren't too fast to be enjoyable
+    delay(30);     //Paces visuals so they aren't too fast to be enjoyable
 }
 
 
-
-void Visualize() {
-  switch (visual) {
-    case 0: return VisualizePalettePulse();
-    case 1: return VisualizeTraffic();
-    case 2: return VisualizeMagnitude();
-    default: return VisualizePalettePulse();
-  }
-}
-
-//This function calls the appropriate color palette based on "palette"
-//  If a negative value is passed, returns the appropriate palette withe "gradient" passed.
-//  Otherwise returns the color palette with the passed value (useful for fitting a whole palette on the strip).
-uint32_t ColorPalette(float num) {
-  switch (palette) {
-    case 0: return (num < 0) ? Rainbow(gradient) : Rainbow(num);
-    case 1: return (num < 0) ? Xmas(gradient) : Rainbow(num);
-    default: return Rainbow(gradient);
-  }
-}
 
 // ##### Visualizations #####
 
-void VisualizeMagnitude()
-{
-    int max_pixel = volume / max_volume * LED_LENGTH;
-
-    for (int i = 0; i < max_pixel; i++)
-    {
-        strip.setPixelColor(i, RED);
-    }
-
-    for (int i = max_pixel; i < LED_LENGTH; i++)
-    {
-        strip.setPixelColor(i, OFF);
-    }
-    
-    strip.show();
-
-    Serial.println(
-        "Vol: " + String(volume) + 
-        ", Avg: " + String(avg_volume) + 
-        ", Max: " + String(max_volume) + 
-        ", Pixel: " + String(max_pixel));
-}
-
-void VisualizePalettePulse() {
-  fade(0.75);
-  if (bump) gradient += thresholds[palette] / 24;
-  if (volume > 0) {
-    int start = LED_HALF - (LED_HALF * (volume / max_volume));
-    int finish = LED_HALF + (LED_HALF * (volume / max_volume)) + strip.numPixels() % 2;
-    for (int i = start; i < finish; i++) {
-      uint32_t shifted = shiftPixel(i);
-      
-      float damp = sin((i - start) * PI / float(finish - start));
-      damp = pow(damp, 2.0);
-
-      //This is the only difference from Pulse(). The color for each pixel isn't the same, but rather the
-      //  entire gradient fitted to the spread of the pulse, with some shifting from "gradient".
-      int val = thresholds[palette] * (i - start) / (finish - start);
-      val += gradient;
-      uint32_t col = ColorPalette(val);
-
-      uint32_t col2 = strip.getPixelColor(shifted);
-      uint8_t colors[3];
-      float avgCol = 0, avgCol2 = 0;
-      for (int k = 0; k < 3; k++) {
-        colors[k] = split(col, k) * damp * knob * pow(volume / max_volume, 2);
-        avgCol += colors[k];
-        avgCol2 += split(col2, k);
-      }
-      avgCol /= 3.0, avgCol2 /= 3.0;
-      if (avgCol > avgCol2) strip.setPixelColor(shifted, strip.Color(colors[0], colors[1], colors[2]));
-    }
-  }
-  strip.show();
-}
 
 //Dots racing into each other
 void VisualizeTraffic() {
 
   //fade() actually creates the trail behind each dot here, so it's important to include.
-  fade(0.8);
+  Fade(0.8);
 
   //Create a dot to be displayed if a bump is detected.
   if (bump) {
@@ -257,9 +186,9 @@ void VisualizeTraffic() {
 
       //Give it a color based on the value of "gradient" during its birth.
       uint32_t col = ColorPalette(-1);
-      gradient += thresholds[palette] / 24;
+      gradient += PALETTE_THRESHOLD / 24;
       for (int j = 0; j < 3; j++) {
-        rgb[slot][j] = split(col, j);
+        rgb[slot][j] = Split(col, j);
       }
     }
   }
@@ -281,10 +210,10 @@ void VisualizeTraffic() {
 
       //Set the dot to its new position and respective color.
       //  I's old position's color will gradually fade out due to fade(), leaving a trail behind it.
-      strip.setPixelColor( shiftPixel(pos[i]), strip.Color(
-                              float(rgb[i][0]) * pow(volume / max_volume, 2.0) * knob,
-                              float(rgb[i][1]) * pow(volume / max_volume, 2.0) * knob,
-                              float(rgb[i][2]) * pow(volume / max_volume, 2.0) * knob)
+      strip.setPixelColor( ShiftPixel(pos[i]), strip.Color(
+                              float(rgb[i][0]) * pow(volume / max_vol, 2.0) * knob,
+                              float(rgb[i][1]) * pow(volume / max_vol, 2.0) * knob,
+                              float(rgb[i][2]) * pow(volume / max_vol, 2.0) * knob)
                           );
     }
   }
@@ -294,8 +223,17 @@ void VisualizeTraffic() {
 
 // ##### Helper Functions #####
 
+//This function calls the appropriate color palette based on "palette"
+//  If a negative value is passed, returns the appropriate palette withe "gradient" passed.
+//  Otherwise returns the color palette with the passed value (useful for fitting a whole palette on the strip).
+uint32_t ColorPalette(float num)
+{
+    return (num < 0) ? Rainbow(gradient) : Rainbow(num);
+}
+
 //Fades lights by multiplying them by a value between 0 and 1 each pass of loop().
-void fade(float damper) {
+void Fade(float damper) 
+{
 
   //"damper" must be between 0 and 1, or else you'll end up brightening the lights or doing nothing.
 
@@ -310,19 +248,17 @@ void fade(float damper) {
     float colors[3]; //Array of the three RGB values
 
     //Multiply each value by "damper"
-    for (int j = 0; j < 3; j++) colors[j] = split(col, j) * damper;
+    for (int j = 0; j < 3; j++) colors[j] = Split(col, j) * damper;
 
     //Set the dampened colors back to their spot.
     strip.setPixelColor(i, strip.Color(colors[0] , colors[1], colors[2]));
   }
 }
 
-
-
 //As mentioned above, split() gives you one 8-bit color value
 //from the composite 32-bit value that the NeoPixel deals with.
 //This is accomplished with the right bit shift operator, ">>"
-uint8_t split(uint32_t color, uint8_t i ) {
+uint8_t Split(uint32_t color, uint8_t i ) {
 
   //0 = Red, 1 = Green, 2 = Blue
 
@@ -332,99 +268,20 @@ uint8_t split(uint32_t color, uint8_t i ) {
   return -1;
 }
 
-uint32_t shiftPixel(uint32_t original) 
+uint32_t ShiftPixel(uint32_t original) 
 {
     return ( original + 60 - LED_OFFSET ) % LED_LENGTH;
 }
 
-//These functions simply take a value and return a gradient color
-//  in the form of an unsigned 32-bit integer
-
-//The gradients return a different, changing color for each multiple of 255
-//  This is because the max value of any of the 3 RGB values is 255, so it's
-//  an intuitive cutoff for the next color to start appearing.
-//  Gradients should also loop back to their starting color so there's no jumps in color.
-
 uint32_t Rainbow(unsigned int i) {
-  if (i > 1529) return Rainbow(i % 1530);
-  if (i > 1274) return strip.Color(255, 0, 255 - (i % 255));   //violet -> red
-  if (i > 1019) return strip.Color((i % 255), 0, 255);         //blue -> violet
-  if (i > 764) return strip.Color(0, 255 - (i % 255), 255);    //aqua -> blue
-  if (i > 509) return strip.Color(0, 255, (i % 255));          //green -> aqua
-  if (i > 255) return strip.Color(255 - (i % 255), 255, 0);    //yellow -> green
+  if (i > 1536) return Rainbow(i % 1536);
+  if (i > 1280) return strip.Color(255, 0, 255 - (i % 256));   //violet -> red
+  if (i > 1024) return strip.Color((i % 256), 0, 255);         //blue -> violet
+  if (i > 768) return strip.Color(0, 255 - (i % 256), 255);    //aqua -> blue
+  if (i > 512) return strip.Color(0, 255, (i % 256));          //green -> aqua
+  if (i > 256) return strip.Color(255 - (i % 256), 255, 0);    //yellow -> green
   return strip.Color(255, i, 0);                               //red -> yellow
 }
-
-uint32_t Xmas(uint32_t i)
-{
-    if (i > 511)
-    {
-        return Xmas(i % 512);
-    }
-    if (i > 255)
-    {
-        return strip.Color(i % 256, 255 - (i % 256), 0);  // green --> red
-    }
-    else
-    {
-        return strip.Color(255 - i, i, 0);   // red -> green
-    }
-}
-
-
-void CyclePalette() {
-
-  //IMPORTANT: Delete this whole if-block if you didn't use buttons//////////////////////////////////
-
-  //If a button is pushed, it sends a "false" reading
-  if (!digitalRead(BUTTON_1)) {
-
-    palette++;     //This is this button's purpose, to change the color palette.
-
-    //If palette is larger than the population of thresholds[], start back at 0
-    //  This is why it's important you add a threshold to the array if you add a
-    //  palette, or the program will cylce back to Rainbow() before reaching it.
-    if (palette >= sizeof(thresholds) / 2) palette = 0;
-
-    gradient %= thresholds[palette]; //Modulate gradient to prevent any overflow that may occur.
-
-    //The button is close to the microphone on my setup, so the sound of pushing it is
-    //  relatively loud to the sound detector. This causes the visual to think a loud noise
-    //  happened, so the delay simply allows the sound of the button to pass unabated.
-    delay(350);
-
-    max_volume = avg_volume;  //Set max volume to average for a fresh experience.
-  }
-
-}
-
-
-void CycleVisual() {
-
-  //IMPORTANT: Delete this whole if-block if you didn't use buttons//////////////////////////////////
-  if (!digitalRead(BUTTON_2)) {
-
-    visual++;     //The purpose of this button: change the visual mode
-
-    gradient = 0; //Prevent overflow
-
-    //Resets "visual" if there are no more visuals to cycle through.
-    if (visual > VISUALS) visual = 0;
-    //This is why you should change "VISUALS" if you add a visual, or the program loop over it.
-
-    //Resets the positions of all dots to nonexistent (-2) if you cycle to the Traffic() visual.
-    if (visual == 2) memset(pos, -2, sizeof(pos));
-
-
-    //Like before, this delay is to prevent a button press from affecting "maxVol."
-    delay(350);
-
-    max_volume = avg_volume; //Set max volume to average for a fresh experience
-  }
-
-
-}
-
 
 
 // ##### Testing Functions #####
@@ -437,5 +294,6 @@ void ColorWipe(uint32_t color, int wait) {
     delay(wait);
   }
 }
-  
+
+
 
